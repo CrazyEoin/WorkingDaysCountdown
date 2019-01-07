f0b4c8ac362047a20bd0a92f1b4ace455d38356f
CrazyEoin <eoinmeagher@gmail.com>
2019-01-07 22:58:07 +0000

    Added a label and now getting it to update correctly.

--- CountDownTimer.py
+++ CountDownTimer.py
@@ -6,7 +6,13 @@ import arrow
 app = QApplication([])
 text_area = QTextEdit()
 label = QLabel()
-label.setText("test")
+label.setText("Initial text")
+
+# self.title = 'This is my title'
+# self.left = 400
+# self.top = 400
+# self.width = 300
+# self.height = 200
 
 layout = QVBoxLayout()
 layout.addWidget(text_area)
@@ -18,13 +24,13 @@ window.show()
 
 
 def display_new_messages():
-    d1 = arrow.get('2012-06-05 16:20:03', 'YYYY-MM-DD HH:mm:ss')
-    d2 = arrow.get(1504384602)
-    text_area.append('d1: '+str(d1))
-    text_area.append('d2: ' + str(d2))
+    # d1 = arrow.get('2012-06-05 16:20:03', 'YYYY-MM-DD HH:mm:ss')
+    # d2 = arrow.get(1504384602)
+    # text_area.append('d1: '+str(d1))
+    # text_area.append('d2: ' + str(d2))
 
     now = arrow.now()
-    end = arrow.get('2019-01-31 16:20:03', 'YYYY-MM-DD HH:mm:ss')
+    end = arrow.get('2019-01-31 00:00:00', 'YYYY-MM-DD HH:mm:ss')
     calc = now - end
     text_area.append(str(calc))
     label.setText(str(calc))
